ea9eeb7ed23c08c038b9dfc895a80d4dea4d78d3
Cxcjl-fly <eevopop@gmail.com>
2026-09-15 16:26:04 +0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// Paste the values from Supabase: Project Settings -> API
+window.OPISKELU_SUPABASE = {
+  url: 'https://YOUR_PROJECT_REF.supabase.co',
+  anonKey: 'YOUR_PUBLISHABLE_OR_ANON_KEY'
+};
